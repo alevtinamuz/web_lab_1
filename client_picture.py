cc1c4518fbf15a014bd8a6d1072f6fa2ceaa8214
alevtinamuz <113981269+alevtinamuz@users.noreply.github.com>
2023-10-10 01:21:36 +1000

    Add files via upload

---
+++ client_picture.py
@@ -0,0 +1,18 @@
+import typing
+from PyQt6 import QtCore
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+import socket
+
+HOST = socket.gethostname()
+PORT = 40808
+
+# class PictureWindow(QMainWindow):
+#     def __init__(self):
+#         super().__init__(parent, flags)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+    client.connect((HOST, PORT))
+    client.sendall(b'I connected.')
+    while True:
+        data = client.recv(1024)
+        print(f"Recieved: {data.decode()}")
